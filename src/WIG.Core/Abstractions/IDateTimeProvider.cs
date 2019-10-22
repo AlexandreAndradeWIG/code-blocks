f3f659075519b48e25e2d06761643b736c63cf5e
antunesl <lr.antunes@gmail.com>
2019-10-22 11:59:13 +0100

    Adds IDateTimeProvider abstraction.

---
+++ src/WIG.Core/Abstractions/IDateTimeProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WIG.Core.Abstractions
+{
+    /// <summary>
+    /// Abstraction for a DateTime providing service.
+    /// This is useful to make the code testable, and make it possible to test code that depends on DateTime.Now in the past or future.
+    /// </summary>
+    public interface IDateTimeProvider
+    {
+        DateTime Now { get; }
+        DateTime UtcNow { get; }
+    }
+}
