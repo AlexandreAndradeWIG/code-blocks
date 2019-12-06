5e58f75d88d7e46d6a4d992df7661ada1cf4e6ed
Luis Antunes <lr.antunes@gmail.com>
2019-12-06 18:05:37 +0000

    Added HttpClient extensions  (missing tests).

---
+++ tests/CodeBlocks.Web.Tests/Http/HttpExtensionsTests.cs
@@ -0,0 +1,25 @@
+﻿using CodeBlocks.Web.Http.Extensions;
+using System.Net.Http;
+using Xunit;
+
+namespace CodeBlocks.Web.Tests.Http
+{
+    public class HttpExtensionsTests
+    {
+        private readonly HttpClient _httpClient;
+
+        public HttpExtensionsTests()
+        {
+            _httpClient = new HttpClient();
+        }
+
+
+        [Theory]
+        [InlineData("https://reqres.in/api/users/2")]
+        public async void GetJsonAsync(string url)
+        {
+            var result = await _httpClient.GetJsonAsync<object>(url, true);
+
+        }
+    }
+}
