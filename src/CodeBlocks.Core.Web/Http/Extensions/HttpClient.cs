10f858b836e294d52f3f12ac10ec8d34aedb7dad
Luis Antunes <lr.antunes@gmail.com>
2019-12-08 13:20:15 +0000

    Adds GetXmlAsync extension method to HtppClient.

--- src/CodeBlocks.Core.Web/Http/Extensions/HttpClient.cs
+++ src/CodeBlocks.Core.Web/Http/Extensions/HttpClient.cs
@@ -1,8 +1,10 @@
 ﻿using Newtonsoft.Json;
 using System;
+using System.IO;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace CodeBlocks.Web.Http.Extensions
 {
@@ -38,6 +40,42 @@ namespace CodeBlocks.Web.Http.Extensions
 
             var responseContent = await response.Content.ReadAsStringAsync();
             var returnObj = JsonConvert.DeserializeObject<TResponse>(responseContent);
+
+            return returnObj;
+        }
+
+        /// <summary>
+        /// Makes a GET request and deserializes a Xml response to a object of type <typeparamref name="TResponse"/>.
+        /// </summary>
+        /// <typeparam name="TResponse">Type of the response.</typeparam>
+        /// <param name="httpClient">HttpClient</param>
+        /// <param name="url">Url for the request.</param>
+        /// <param name="throwOnError">If true, the method will throw an Exception when the StatusCode of the response wasn't in 200-299 range.</param>
+        /// <returns></returns>
+        public static async Task<TResponse> GetXmlAsync<TResponse>(this HttpClient httpClient, string url, bool throwOnError = false)
+        {
+            var response = await httpClient.GetAsync(url);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                if (throwOnError)
+                {
+                    if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+                    {
+                        throw new UnauthorizedAccessException(response.ReasonPhrase);
+                    }
+                    throw new Exception(response.ReasonPhrase);
+                }
+                else
+                {
+                    return default;
+                }
+            }
+
+            var responseContent = await response.Content.ReadAsStringAsync();
+            XmlSerializer serializer = new XmlSerializer(typeof(TResponse));
+            StringReader stringReader = new StringReader(responseContent);
+            var returnObj = (TResponse)serializer.Deserialize(stringReader);
 
             return returnObj;
         }
