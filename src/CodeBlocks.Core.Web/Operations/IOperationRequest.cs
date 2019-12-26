08852c78a72397018b9fb97a5cbc9fff5deaf4eb
Luis Antunes <lr.antunes@gmail.com>
2019-12-26 19:38:08 +0000

    Adds IPagingOperationRequest

--- src/CodeBlocks.Core.Web/Operations/IOperationRequest.cs
+++ src/CodeBlocks.Core.Web/Operations/IOperationRequest.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CodeBlocks.Core.Web.Operations
+﻿namespace CodeBlocks.Core.Web.Operations
 {
     public interface IOperationRequest
     {
-        
+
+    }
+
+    public interface IPagingOperationRequest
+    {
+        int Page { get; }
+        int PageSize { get; }
     }
 }
